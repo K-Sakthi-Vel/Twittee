190e88ce51920f8805c6f77bcaa2389417d27820
K SAKTHIVEL <klsakthi333@gmail.com>
2024-06-19 07:07:21 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,36 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1/Twittee");
+const mongoose=require("mongoose");
+mongoose.connect(
+  'mongodb+srv://mechonsakthi44:f6MJbMOlEBfjrE50@cluster0.0uqjmor.mongodb.net/',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
-db.on("error",console.error.bind(console,"Error in connecting to DB"));
+db.on('error', console.error.bind('console', 'error'));
 
-db.once("open", () => {
-    console.log("Connected to MongoDB :)")
-})
+db.once('open', function () {
+  console.log('welcome connect to database');
+});
 
 module.exports = db;
+
+
+
+
+
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://127.0.0.1/Twittee");
+
+// const db = mongoose.connection;
+
+// db.on("error",console.error.bind(console,"Error in connecting to DB"));
+
+// db.once("open", () => {
+//     console.log("Connected to MongoDB :)")
+// })
+
+// module.exports = db;
